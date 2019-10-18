d73d9996de328ea75498704f39701cd5b053d04a
Andrew Swift <andrew@svija.com>
2019-10-18 15:47:46 +0200

    fixing (but not finished) choose_language.js

--- svija/templates/svija/javascript/choose_language.js
+++ svija/templates/svija/javascript/choose_language.js
@@ -5,23 +5,36 @@ function chooseLanguage(flag){
   location.href=flag;
 }
 
-// redirect to other language?
-// needs to be rewritten to use prefixes from DB
+// redirect if 4 conditions are met
+
+// condition 1: visitor not already on site
 
 var x = 'h'+document.referrer;
-refr_parts = x.split('/');
-page_parts = page_url.split('/');
+var refr_parts = x.split('/');
+var page_parts = page_url.split('/');
+var cond1 = refr_parts[2] != page_parts[2];
 
-// need to add check for NOT internal page to following IF statement
+// condition 2: there's no subdirectory
 
-// if visitor is not already on site
-if (refr_parts[2] != page_parts[2]){
-  if (getCookie('choseLang') == '/fr/' && location.href.indexOf('/en/') > 0) { location.href = '/fr/'; }
-  if (getCookie('choseLang') == '/en/' && location.href.indexOf('/fr/') > 0) { location.href = '/en/'; }
+var bits = location.href.split('/');
+var cond2 = bits.length == 4;
 
-  if (getCookie('choseLang') == '/fr/' && location.href.indexOf('/em/') > 0) { location.href = '/fm/'; }
-  if (getCookie('choseLang') == '/en/' && location.href.indexOf('/fm/') > 0) { location.href = '/em/'; }
+// condition 3: cookie choice is not same as default prefix
+//              or there is no cookie choice
+
+var cky = getCookie('choseLang');
+var pfx = '/'+default_site_prefix+'/';
+if (cky == ''){ cky = pfx; }
+var cond3 = cky != pfx;
+
+// condition 4: we're not already on mobile version of correct language if desktop is default or desktop if mobile is default
+// this could only happen if mobile version was default prefix
+// chooseLanguage should choose language, not prefix
+// then I could get prefixes & check all of them
+
+var cond4 = false
+
+// needs to be rewritten to use prefixes from DB
+if (cond1 && cond2 && cond3 && cond4){
+  location.href = cky;
 }
-
-// need to know default site language & default language prefix to be able to fix this
-// right now it will have no effect because it is only called 
