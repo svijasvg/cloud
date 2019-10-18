1ffd9e68cbde72e03d67b2db174a7a4749e83e87
Andrew Swift <andrew@svija.com>
2019-10-18 15:05:59 +0200

    fixing language choice redirection

--- svija/templates/svija/javascript/choose_language.js
+++ svija/templates/svija/javascript/choose_language.js
@@ -8,8 +8,20 @@ function chooseLanguage(flag){
 // redirect to other language?
 // needs to be rewritten to use prefixes from DB
 
-if (getCookie('choseLang') == '/fr/' && location.href.indexOf('/en/') > 0) { location.href = '/fr/'; }
-if (getCookie('choseLang') == '/en/' && location.href.indexOf('/fr/') > 0) { location.href = '/en/'; }
+var x = 'h'+document.referrer;
+refr_parts = x.split('/');
+page_parts = page_url.split('/');
 
-if (getCookie('choseLang') == '/fr/' && location.href.indexOf('/em/') > 0) { location.href = '/fm/'; }
-if (getCookie('choseLang') == '/en/' && location.href.indexOf('/fm/') > 0) { location.href = '/em/'; }
+// need to add check for NOT internal page to following IF statement
+
+// if visitor is not already on site
+if (refr_parts[2] != page_parts[2]){
+  if (getCookie('choseLang') == '/fr/' && location.href.indexOf('/en/') > 0) { location.href = '/fr/'; }
+  if (getCookie('choseLang') == '/en/' && location.href.indexOf('/fr/') > 0) { location.href = '/en/'; }
+
+  if (getCookie('choseLang') == '/fr/' && location.href.indexOf('/em/') > 0) { location.href = '/fm/'; }
+  if (getCookie('choseLang') == '/en/' && location.href.indexOf('/fm/') > 0) { location.href = '/em/'; }
+}
+
+// need to know default site language & default language prefix to be able to fix this
+// right now it will have no effect because it is only called 
