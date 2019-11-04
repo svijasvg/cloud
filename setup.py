b9705827bb3e9ae58498e7f264f5b445e892ba12
Andrew Swift <andrew@svija.com>
2019-11-04 16:24:17 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-svija',
-    version='2.0.16',
+    version='2.0.17',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
