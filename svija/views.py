ca57b53ea074602ad9a058de2ba0a0059946345a
Andrew Swift <andrew@svija.com>
2019-11-07 15:09:27 +0100

    extracted svg treatment to function

--- svija/views.py
+++ svija/views.py
@@ -428,28 +428,9 @@ def PageView(request, path1, path2):
     all_svgs  = page.svg_set.all()
     svg = ''
 
-    for this_svg in all_svgs:
-        if this_svg.active:
-
-            #—————— check if svg exists
-            temp_source = os.path.abspath(os.path.dirname(__name__)) + '/' + source_dir + '/' + this_svg.filename
-            path = pathlib.Path(temp_source)
-            if not path.exists():
-                return error404(request)
-
-            svg_ID, svg_width, svg_height, svg_content = svg_cleaner.clean(temp_source, this_svg.filename)
-    
-            if svg_width > specified_width:
-                page_ratio = svg_height/svg_width
-                svg_width = specified_width
-                svg_height = round(specified_width * page_ratio)
-
-            rem_width = svg_width/10
-            rem_height = svg_height/10
-    
-            css_dims = '#' + svg_ID + '{ width:' + str(rem_width) + 'rem; height:' + str(rem_height) + 'rem; }'
-            head_css += '\n\n' + css_dims
-            svg += '\n' + svg_content
+    thisThing = my_special_function(source_dir, all_svgs, specified_width)
+    svg += thisThing[1]
+    head_css += thisThing[0]
 
     #———————————————————————————————————————— library scripts
 
@@ -594,5 +575,72 @@ def add_script(kind, name, content):
         'css' : '\n\n/* '   + name + ' */\n'  + content,
         'js'  : '\n\n// '   + name + '\n'     + content,
     }[kind]
+
+#———————————————————————————————————————— fin
+# line 431:
+
+def my_special_function(source_dir, all_svgs, specified_width):
+    svg = ''
+    head_css = ''
+    for this_svg in all_svgs: #WHERE ACTIVE == TRUE, ORDER BY LOAD_ORDER
+        if this_svg.active:
+
+            #—————— check if svg exists
+            temp_source = os.path.abspath(os.path.dirname(__name__)) + '/' + source_dir + '/' + this_svg.filename
+            path = pathlib.Path(temp_source)
+            if not path.exists():
+                return error404(request)
+
+            svg_ID, svg_width, svg_height, svg_content = svg_cleaner.clean(temp_source, this_svg.filename)
+    
+            if svg_width > specified_width:
+                page_ratio = svg_height/svg_width
+                svg_width = specified_width
+                svg_height = round(specified_width * page_ratio)
+
+            rem_width = svg_width/10
+            rem_height = svg_height/10
+    
+            css_dims = '#' + svg_ID + '{ width:' + str(rem_width) + 'rem; height:' + str(rem_height) + 'rem; }'
+            head_css += '\n\n' + css_dims
+            svg += '\n' + svg_content
+    results = []
+    results.append(head_css)
+    results.append(svg)
+    return results
+
+#line 514:
+#
+#        for this_svg in all_svgs:
+#    
+#            #—————— check if svg exists
+#            temp_source = os.path.abspath(os.path.dirname(__name__)) + '/' + source_dir + '/' + this_svg.filename
+#            path = pathlib.Path(temp_source)
+#            if not path.exists():
+#                return error404(request)
+#    
+#            svg_ID, svg_width, svg_height, svg_content = svg_cleaner.clean(temp_source, this_svg.filename)
+#    
+#            if svg_width > specified_width:
+#                page_ratio = svg_height/svg_width
+#                svg_width = specified_width
+#                svg_height = round(specified_width * page_ratio)
+#    
+#            rem_width = svg_width/10
+#            rem_height = svg_height/10
+#    
+#            css_dims = '#' + svg_ID + '{ width:' + str(rem_width) + 'rem; height:' + str(rem_height) + 'rem; }'
+#    
+#            head_css += '\n\n' + css_dims
+#            module += '\n' + svg_content
+#    
+#            all_scripts = this_svg.modulescripts_set.all()
+#            for this_script in all_scripts:
+#                if this_script.type == 'CSS' and this_script.active == True:
+#                    head_css += add_script('css', this_script.name, this_script.content)
+#                if this_script.type == 'head JS' and this_script.active == True:
+#                    user_js += add_script('js', this_script.name, this_script.content)
+#                if this_script.type == 'body JS' and this_script.active == True:
+#                    body_js += add_script('js', this_script.name, this_script.content)
 
 #———————————————————————————————————————— fin
