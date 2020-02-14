352128a97ce2849b94d9cf60844a5ab40916bb06
Andrew Swift <andrew@svija.com>
2020-02-14 09:47:50 +0100

    added version 2.1.2

--- save_tar.sh
+++ save_tar.sh
@@ -1,4 +1,5 @@
-source ../Env/djangoEnv/bin/activate
-git add dist/django-svija-2.1.1.tar.gz
-git commit -m "added version 2.1.1" -a
+#source ../Env/djangoEnv/bin/activate
+workon djangoEnv
+git add dist/django-svija-2.1.2.tar.gz
+git commit -m "added version 2.1.2" -a
 git push origin master
