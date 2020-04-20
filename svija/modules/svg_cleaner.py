945381cee3a15417540acc3374639e768c5184b0
Andrew Swift <andrew@svija.com>
2020-04-20 15:17:50 +0200

    cleaned & refactored in preparation for font management

--- svija/modules/svg_cleaner.py
+++ svija/modules/svg_cleaner.py
@@ -16,19 +16,19 @@ import os, re, io
 from svija.models import Font
 from django.core.exceptions import ObjectDoesNotExist
 
-def clean(svg_path, svg_name):
+def clean(file_path, file_name):
 
     # if unspecified, ID will be filename with extension removed (-en.svg)
-    svg_ID         = name_cleanup(svg_name)
-    add_ID         = True
+    svg_ID         = cleanup(file_name)
     width = height = 0
-    line_number    = -1
+    line_number    = 2
+    first_line     = ''
     final_svg      = ''
-    new_fonts, fonts_file, fonts_goog = [], [], []
 
     #———————————————————————————————————— list of woff & google fonts in DB
     # could be rewritten in three lines
 
+    new_fonts, fonts_file, fonts_goog = [], [], []
     fonts_db = Font.objects.all()
     for this_font in fonts_db:
         if this_font.google:
@@ -36,37 +36,31 @@ def clean(svg_path, svg_name):
         else:
             fonts_file.append(this_font)
 
-    with open(svg_path, 'r', encoding='utf-8') as f:
+    #———————————————————————————————————— read in the SVG file
+
+    with open(file_path, 'r', encoding='utf-8') as f:
         raw_svg = f.read()
         svg_lines = raw_svg.split('\n')
 
     #———————————————————————————————————— main loop to process SVG line by line
 
+    lines_quantity = len(svg_lines)
     while True:
 
-        line_number = line_number + 1
-        if line_number == len(svg_lines): break # we're done
+        if line_number == lines_quantity-1: break # we're done
+        line = svg_lines[line_number]
 
-        line = svg_lines[line_number - 1]
-        if line[0:5] == '<?xml': continue
-        if line[0:4] == '<!--': continue
+        #———————————————————————————————— keep 1st line to update ID when done
 
-        #———————————————————————————————— if svg has an ID, use it
-
-        # single-layer AI docs have svg ID named for layer
-        # <svg version="1.1" id="Layer_1" xmlns="http:
-        if line[0:4] == '<svg':
-            if line.find('id="') > 0:
-                parts = line.split('"')
-                svg_ID = parts[3]
-                add_ID = False
+        if line_number == 2:
+            first_line = line
 
         #———————————————————————————————— get dimensions from viewbox value in svg tag
 
         # <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="260.1px"
         #	 height="172.6px" viewBox="0 0 260.1 172.6" style="enable-background:new 0 0 260.1 172.6;" xml:space="preserve">
 
-        if line_number < 5 and line.find('viewBox') > 0:
+        if line_number == 3:
             parts1 = line.split('viewBox="')
             parts2 = parts1[1].split('" ')
             viewBox = parts2[0]
@@ -74,51 +68,24 @@ def clean(svg_path, svg_name):
             px_width = float(dimensions[2])
             px_height = float(dimensions[3])
 
-        #————————————————————————————————— find fonts
-        # .stLayer_12{font-family:'Signika-Regular';}
-        #————————————————————————————————————————————
-
-        if line[1:4] == '.st' and line.find('font-family') > 0:
-            line_parts = line.split("'")
-            css_ref = line_parts[1]
-
-            # if it is a google font already in DB
-            # replace Illustrator-style def with Google's
-            google_font = [x for x in fonts_goog if x.name == css_ref]
-            woff_font = [x for x in fonts_file if x.name == css_ref]
-
-            if len(google_font) > 0:
-                line_parts[1] = redefine_styles(google_font, line_parts[1])
-                line = ''.join(line_parts)
-
-            # if it's not a woff font already in DB the add it
-            elif len(woff_font) <= 0:
-                # should be a better way to do this
-                font_to_replace = Font()
-                new_font = create_new_font(css_ref, font_to_replace)
-                new_fonts.append(new_font)
-
         #———————————————————————————————— replace '.st0' style definitions at top of SVG
-        #———————————————————————————————— and style applications in body of SVG
 
         if line[1:4] == '.st':
             parts = line.split('.st')
             line = '\t.st' + svg_ID + parts[1]
 
         #———————————————————————————————— change <path class="st2" to <path class="st[id]2"
+        #———————————————————————————————— change <rect id="SVGID_53_" to <rect id="[id]_53_"
 
         if line.find('class="st') > 0:
             line = re.sub(r'([\"," "])st([0-9]*)(?=[\"," "])', r'\1st'+svg_ID+r'\2', line)
-
-        #———————————————————————————————— change <rect id="SVGID_53_" to <rect id="[id]_53_"
 
         if line.find('SVGID_') > 0:
             line = re.sub(r'SVGID_', r''+svg_ID+'_', line)
 
         #———————————————————————————————— fix mixed text weight problem
         #
-        # in the following, if the x value is not 0 I need to delete x & y coords
-        # <tspan x="400.88" y="147">some text</tspan>
+        # delete coords for <tspan x="400.88" y="147">some text</tspan>
 
         # regex = tspan x=\"[1-9]
         exp = r'tspan x=\"[1-9]'
@@ -127,22 +94,45 @@ def clean(svg_path, svg_name):
           line = regex.sub('tspan', line)
 
         #———————————————————————————————— get id if layer like "id example" exists
+        # note that this means the ID could change at the end, so .st[id]8 won't correspond
 
         if line[1:10] == 'g id="id_':
             parts = line.split('"')
             svg_ID = parts[1][3:]
 
-     #———————————————————————————————— add line into final SVG
+        #————————————————————————————————— find fonts
+        # .stLayer_12{font-family:'Signika-Regular';}
+        #————————————————————————————————————————————
 
-        final_svg += '\n' + line;
+        if line[1:4] == '.st':
+            if line.find('family') > 0:
+                line_parts = line.split("'")
+                css_ref = line_parts[1]
 
-  #———————————————————————————————————————— add new ID if necessary
+                # if it is a google font already in DB
+                # replace Illustrator-style def with Google's
+                google_font = [x for x in fonts_goog if x.name == css_ref]
+                woff_font = [x for x in fonts_file if x.name == css_ref]
 
-    if add_ID:
-        final_svg = final_svg.replace('<svg ', '<svg id="' + svg_ID + '" ', 1)
+                if len(google_font) > 0:
+                    line_parts[1] = redefine_styles(google_font, line_parts[1])
+                    line = ''.join(line_parts)
 
-  #———————————————————————————————————————— check font table
-  # https://stackoverflow.com/questions/14676613/how-to-import-google-web-font-in-css-file
+                # if it's not a woff font already in DB the add it
+                elif len(woff_font) <= 0:
+                    # should be a better way to do this
+                    font_to_replace = Font()
+                    new_font = create_new_font(css_ref, font_to_replace)
+                    new_fonts.append(new_font)
+
+        #————————————————————————————— close main loop
+
+        if line_number > 2:
+            final_svg += '\n' + line;
+        line_number += 1
+
+    #—————————————————————————————————————— check font table
+    # https://stackoverflow.com/questions/14676613/how-to-import-google-web-font-in-css-file
 
     # add fonts that were not already in DB to DB
     for each_font in new_fonts:
@@ -153,9 +143,24 @@ def clean(svg_path, svg_name):
             p = Font.objects.create(name = each_font.name, family = each_font.family, style=each_font.style, source=each_font.source, google=False, active=False)
             p.save
 
-    return svg_ID, px_width, px_height, final_svg
+    #—————————————————————————————————————— add new ID if necessary
 
+    # only single-layer AI docs have an ID
+    # <svg version="1.1" id="Layer_1" xmlns="http:
+
+    if first_line.find('id="') > 0:
+        parts = line.split('"')
+        svg_ID = parts[3]
+    else:
+        first_line = first_line.replace('<svg ', '<svg id="' + svg_ID + '" ', 1)
+
+  #———————————————————————————————————————— return SVG ID, dimensions & contents
+
+    return svg_ID, px_width, px_height, first_line+final_svg
+
+#—————————————————————————————————————————————————— 
 #———————————————————————————————————————— functions
+#—————————————————————————————————————————————————— 
 
 def create_new_font(css_ref, new_font):
 
@@ -238,9 +243,9 @@ def redefine_styles(google_font, style_string):
     style_string = style_string[0:-1]
     return style_string
 
-#———————————————————————————————————————— fin
+#———————————————————————————————————————— remove special characters
 
-def name_cleanup(filename):
+def cleanup(filename):
 # per.iod in na,me.svg
   translation_table = dict.fromkeys(map(ord, ' \'",.!@#$'), '-')
   filename = filename.translate(translation_table)
