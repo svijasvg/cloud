000c54d669ff7eafbd55b6e450c58010474c7e3a
[svijasvg] <[andrew@svija.com]>
2020-06-17 12:12:04 +0200

    all working now

--- svija/views/SitemapView.py
+++ svija/views/SitemapView.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from svija.models import Settings, Page
 
-from svija.modules import sitemap
+from modules import sitemap
 
 def SitemapView(request):
     settings = get_object_or_404(Settings,active=True)
