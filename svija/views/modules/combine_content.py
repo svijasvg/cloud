392b9ef8db7c6006c0943ff464b7463d78bbe053
[svijasvg] <[andrew@svija.com]>
2020-06-29 21:02:33 +0200

    consolidation almost complete

--- svija/views/modules/combine_content.py
+++ svija/views/modules/combine_content.py
@@ -10,21 +10,19 @@ from PageView import page_obj
 def combine_content(content_blocks):
 
     combined_content = {
-        'head_js':'', 'css':'', 'body_js':'', 'svgs':'', 'html':'', }
+        'js':'', 'css':'', 'body':'', }
 
     head_js = css =  body_js = svgs = html = form = ''
 
     head_js    += ''.join([i['head_js']    for i in content_blocks])
     css        += ''.join([i['css']        for i in content_blocks])
     body_js    += ''.join([i['body_js']    for i in content_blocks])
-    svgs       += ''.join([i['svgs']       for i in content_blocks])
+    html       += ''.join([i['svgs']       for i in content_blocks])
     html       += ''.join([i['html']       for i in content_blocks])
     html       += ''.join([i['form']       for i in content_blocks])
 
-    combined_content['head_js']    += head_js
-    combined_content['css']        += css
-    combined_content['body_js']    += body_js
-    combined_content['svgs']       += svgs
-    combined_content['html']       += html
+    combined_content['js']    += head_js
+    combined_content['css']   += css
+    combined_content['body']  += html + '<script>' + body_js + '</script>'
 
     return combined_content
