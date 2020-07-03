e7b31aa550089fe078770ab1bf7abb804defdf15
[svijasvg] <[andrew@svija.com]>
2020-07-03 11:50:55 +0200

    added version 2.1.5

--- save_tar.sh
+++ save_tar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /opt/Env/djangoEnv/bin/activate
+source /opt/venv/djangoEnv/bin/activate
 python setup.py sdist
 git add dist/django-svija-2.1.5.tar.gz
 git commit -m "added version 2.1.5" -a
