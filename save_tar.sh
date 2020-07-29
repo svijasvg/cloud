2cd4cece5e9e2d0144b150620225d95c2da51376
[svijasvg] <[andrew@svija.com]>
2020-07-29 14:50:59 +0200

    added tar.gz for version 2.1.6

--- save_tar.sh
+++ save_tar.sh
@@ -2,5 +2,5 @@
 source /opt/venv/djangoEnv/bin/activate
 python setup.py sdist
 git add dist/django-svija-2.1.6.tar.gz
-git commit -m "added version 2.1.6" -a
+git commit -m "added tar.gz for version 2.1.6" -a
 git push origin master
