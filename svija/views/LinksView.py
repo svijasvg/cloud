6bd381d589195c067f27449ffb9cbac1f1c7eb38
[andrewcswift] <[andrew@svija.love]>
2021-09-13 10:42:35 +0200

    home page images working, check other pages next

--- svija/views/LinksView.py
+++ svija/views/LinksView.py
@@ -9,25 +9,34 @@ from django.shortcuts import get_object_or_404
 from svija.models import Prefix, PrefixModules, Settings
 
 import os
-SITE_ROOT = os.path.realpath(os.path.dirname(__file__)+'/../')
+
+sync_folder = os.path.abspath(os.path.dirname(__name__)) + '/sync/'
 
 def LinksView(request, request_prefix, placed_file):
 
     # full path is in request.path for free
 
-    return HttpResponse("debugging message: " + request_prefix + '::' + placed_file)
-    try:
-        prefix = Prefix.objects.get(path=request_prefix)
-    except ObjectDoesNotExist:
-        settings = get_object_or_404(Settings,active=True)
-        prefix = settings.prefix
+    # home page image path ../../links/demo-only-big-29258056.jpg
 
-    source_dir = os.path.abspath(os.path.dirname(__name__)) + '/sync'
-    source_dir += '/Links/' + placed_file
+#   return HttpResponse("⚠️ " + sync_folder + request_prefix + '/' + placed_file)
+
+#   try:
+#       prefix = Prefix.objects.get(path=request_prefix)
+#   except ObjectDoesNotExist:
+#       settings = get_object_or_404(Settings,active=True)
+#       prefix = settings.prefix
+
+#   source_dir = os.path.abspath(os.path.dirname(__name__)) + '/sync'
+#   source_dir += '/Links/' + placed_file
+
+    source_dir = sync_folder + request_prefix + '/' + placed_file
     bits = placed_file.split('.')
     type = bits[-1].lower()
     if type != 'png' and type != 'gif':
         type = 'jpeg'
+
+#———————————————————————————————————————— return file contents
+
     image_data = open(source_dir, "rb").read()
     return HttpResponse(image_data, content_type='image/' + type)
 
