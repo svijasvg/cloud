a3f1f613b06500f9a17245c2d1dbb3c9db6e0015
[andrewcswift] <[andrew@svija.love]>
2021-10-17 14:36:10 +0200

    minor mods

--- svija/views/modules/get_page_svgs.py
+++ svija/views/modules/get_page_svgs.py
@@ -13,7 +13,7 @@ def get_page_svgs(screen_code, page, page_width, use_p3):
     # also edit modules/generate_system_js.py
     for this_svg in all_svgs: #WHERE ACTIVE == TRUE, ORDER BY LOAD_ORDER
         if this_svg.active:
-            s, c = get_single_svg(this_svg, screen_code, page_width, use_p3)
+            s, c = get_single_svg(page, this_svg, screen_code, page_width, use_p3)
             svgs += s
             css += c
 
