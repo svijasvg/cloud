7856ddacd0213c5f2de0acef1f4b4074b62686a0
[andrewcswift] <[andrew@svija.love]>
2021-10-18 21:30:47 +0200

    removed unused reference to scripts folder

--- svija/urls.py
+++ svija/urls.py
@@ -62,7 +62,7 @@ urlpatterns = [
     re_path(r'^fonts/(?P<path>.*)$(?i)',   static.serve, {'document_root': proj_folder + "/sync/Svija/Fonts/WOFF Files"   }),
     re_path(r'^files/(?P<path>.*)$(?i)',   static.serve, {'document_root': proj_folder + "/sync/Svija/Shared Files"       }),
     re_path(r'^images/(?P<path>.*)$(?i)',  static.serve, {'document_root': proj_folder + "/sync/Svija/Icons"              }),
-    re_path(r'^scripts/(?P<path>.*)$(?i)', static.serve, {'document_root': proj_folder + "/sync/Svija/Site Scripts"       }),
+#   re_path(r'^scripts/(?P<path>.*)$(?i)', static.serve, {'document_root': proj_folder + "/sync/Svija/Site Scripts"       }),
 
 
 ]
