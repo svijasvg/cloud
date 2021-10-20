e8de3348d21045de247afecf09c162298c085696
[andrewcswift] <[andrew@svija.love]>
2021-10-20 09:46:34 +0200

    escaped strings in form JS

--- svija/views/modules/generate_form_js.py
+++ svija/views/modules/generate_form_js.py
@@ -15,20 +15,20 @@ def generate_form_js(language):
 
     form_js = '\n//———————————————————————————————————————— mail form\n\n'
 
-    form_js += 'var form_dest = "'       + language.email           + '";\n'
-    form_js += 'var form_subject = "'    + language.subject         + '";\n'
-    form_js += 'var form_fromlable = "'  + language.mail_frm        + '";\n'
+    form_js += 'var form_dest = "'       + scape(language.email          ) + '";\n'
+    form_js += 'var form_subject = "'    + scape(language.subject        ) + '";\n'
+    form_js += 'var form_fromlable = "'  + scape(language.mail_frm       ) + '";\n'
     
-    form_js += 'var name_init = "'       + language.form_name       + '";\n'
-    form_js += 'var business_init = "'   + language.form_business   + '";\n'
-    form_js += 'var address_init = "'    + language.form_email      + '";\n'
-    form_js += 'var message_init = "'    + language.form_status     + '";\n'
-    form_js += 'var send_init = "'       + language.form_send       + '";\n'
+    form_js += 'var name_init = "'       + scape(language.form_name      ) + '";\n'
+    form_js += 'var business_init = "'   + scape(language.form_business  ) + '";\n'
+    form_js += 'var address_init = "'    + scape(language.form_email     ) + '";\n'
+    form_js += 'var message_init = "'    + scape(language.form_status    ) + '";\n'
+    form_js += 'var send_init = "'       + scape(language.form_send      ) + '";\n'
 
-    form_js += 'var status_sending = "'  + language.form_sending    + '";\n'
-    form_js += 'var status_failed = "'   + language.form_alert_fail + '";\n'
-    form_js += 'var status_received = "' + language.form_rcvd       + '";\n'
-    form_js += 'var alert_received = "'  + language.form_alert_rcvd + '";\n'
+    form_js += 'var status_sending = "'  + scape(language.form_sending   ) + '";\n'
+    form_js += 'var status_failed = "'   + scape(language.form_alert_fail) + '";\n'
+    form_js += 'var status_received = "' + scape(language.form_rcvd      ) + '";\n'
+    form_js += 'var alert_received = "'  + scape(language.form_alert_rcvd) + '";\n'
 
 #var address_failed = 'cxoxnxtxaxcxtx@xoxzxaxkxex.xcxom';
 
@@ -46,4 +46,11 @@ def generate_form_js(language):
 #   return core_content
 
     return form_js
+
+#———————————————————————————————————————— function
+
+def scape(str):
+  str = str.replace('"', '\\"')
+  str = str.replace("'", "\\'")
+  return str
 
