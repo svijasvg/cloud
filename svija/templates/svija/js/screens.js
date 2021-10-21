afef6177f361bf640842dd649b83a4643f1ab5f7
[andrewcswift] <[andrew@svija.love]>
2021-10-21 12:26:15 +0200

    reduced delay for page reload

--- svija/templates/svija/js/screens.js
+++ svija/templates/svija/js/screens.js
@@ -31,4 +31,4 @@ for (const [key, value] of Object.entries(all_screens)) {
 setCookie('screen_code', correct_screen_code, 7);
 
 if (screen_code != correct_screen_code)
-  setTimeout(window.location.reload.bind(window.location), 50);
+  setTimeout(window.location.reload.bind(window.location), 5);
