ee896dbfde775a6cbfa783582ccb1b9629ddf7fb
[andrewcswift] <[andrew@svija.love]>
2021-10-28 18:15:06 +0200

    updated version number

--- save_tar.sh
+++ save_tar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/venv/djangoEnv/bin/activate
 python setup.py sdist
-git add dist/django-svija-2.2.8.tar.gz
-git commit -m "added tar.gz for version 2.2.8" -a
+git add dist/django-svija-2.2.9.tar.gz
+git commit -m "added tar.gz for version 2.2.9" -a
 git push origin master
