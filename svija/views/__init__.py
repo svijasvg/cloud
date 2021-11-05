14089fe303861e12f3457d63cdf80a19973c5f48
[andrewcswift] <[andrew@svija.love]>
2021-11-05 11:28:55 +0100

    updated version number

--- svija/views/__init__.py
+++ svija/views/__init__.py
@@ -1,6 +1,6 @@
 #———————————————————————————————————————— __init__.py
 
-version = '2.2.9'
+version = '2.2.10'
 import os, sys
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), './'))
