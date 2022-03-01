5177e8a90a2d0001bd04caf65a580024de8b4c90
[andrewcswift] <[andrew@svija.love]>
2022-03-01 14:59:31 +0100

    changed refrence from font.css to font.svg_ref

--- svija/views/modules/get_fonts.py
+++ svija/views/modules/get_fonts.py
@@ -16,7 +16,7 @@ def get_fonts():
 
     for this_font in font_objs:
         if this_font.active:
-            font_face = this_font.css
+            svg_ref = this_font.svg_ref
             font_src  = this_font.source
 
             if this_font.google:
@@ -27,26 +27,23 @@ def get_fonts():
             elif font_src.find('woff2') > 0:
                 font_format = " format('woff2')"
                 font_src = "url('/fonts/" + font_src
-                font_css  += '\n'+ css_str.format(font_face, font_src, font_format)
+                font_css  += '\n'+ css_str.format(svg_ref, font_src, font_format)
 
             elif font_src.find('woff') > 0:  
                 font_format = " format('woff')"
                 font_src = "url('/fonts/" + font_src
-                font_css += '\n'+ css_str.format(font_face, font_src, font_format)
+                font_css += '\n'+ css_str.format(svg_ref, font_src, font_format)
 
             elif font_src.find(',') > 0: # local fonts
                 # src: local('Arial'), local('Arial MT'), local('Arial Regular'); }
                 font_format = ''
                 locals = font_src.replace(', ',',').split(',')
                 font_src = "local('"+"'), local('".join(locals)
-                font_css += '\n'+ css_str.format(font_face, font_src, font_format)
+                font_css += '\n'+ css_str.format(svg_ref, font_src, font_format)
 
 
     if len(google_fonts) > 0:
         link_str = '  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family={}">'
         font_link = link_str.format(('|').join(google_fonts))
 
-    #ore_content['meta_fonts'] = font_link
-    #ore_content['css'] = font_css + core_content['css']
-    #eturn core_content
     return font_link, font_css
