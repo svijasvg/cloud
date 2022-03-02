c89bbaa29cf252d9ef7522a5c686aaba560f2035
[andrewcswift] <[andrew@svija.love]>
2022-03-02 15:17:36 +0100

    removed all variables from MailView so it only gets message

--- svija/views/MailView.py
+++ svija/views/MailView.py
@@ -1,17 +1,18 @@
 #———————————————————————————————————————— MailView.py
-
+#
+#   remove references to address or name
+#   sender_info = language.mail_frm + ' ' + name + ' (' + addr + ')\n\n'
+#   message = sender_info + message 
+#
 #———————————————————————————————————————— notes
 #
-#   accepts three POST variables
-#
-#   name   email   message
+#   accepts one POST variable: message
 #
 #   returns '' if successful
 #
-#   E0 = missing email or message
-#   E1 = email failes whitelist
-#   E2 = name failed blacklist
-#   E3 = body failed blacklist
+#   or returns
+#   E1 = missing email or message
+#   E2 = message failed blacklist
 #
 #———————————————————————————————————————— import
 
@@ -25,106 +26,55 @@ from modules import send_mail
 
 def MailView(request):
 
-  # comment out these lines for testing
+  settings  = Settings.objects.filter(active=True).first()
+  language  = settings.language
+  blacklist = ".*[\\|\^|\$|\||\*|\+|\[|\{|<|>]+.*"
+
+#———————————————————————————————————————— check for validity
 
   if request.method != 'POST':
     response = HttpResponse(0)
     response.status_code = 404
     return response
 
-  addr = request.POST.get('email').lower()
-  naim = str(request.POST.get('name'))
-  body = request.POST.get('message')
+  message = request.POST.get('message')
 
-# addr = "hompty.hooby@freedom.org"
-# naim = ""
-# body = "me`'ssage body"
+  if message=='':
+    return HttpResponse('E1')
 
-#———————————————————————————————————————— setup
+  if re.match(blacklist, message):
+    return HttpResponse('E2')
 
-  settings = Settings.objects.filter(active=True).first()
-  fail = '' # mail will be sent if not '' at end
+#———————————————————————————————————————— get language from referrer
 
-#———————————————————————————————————————— language
+  referrer = request.META.get('HTTP_REFERER') # https://svija.love/en/try
+  if referrer.count('/') > 3:
+    parts = referrer.split('/')
+    referring_code = parts[3]
+    ref_language = Language.objects.filter(code=referring_code).first()
+    if type(ref_language) is not type(None):
+      language = ref_language
 
-
-  if 'language' in request.POST:
-    furn_code = request.POST.get('language')
-    language = Language.objects.filter(code=furn_code).first()
-  else:
-   language = settings.language
+#———————————————————————————————————————— language-dependent parameters
 
   to       = language.email
   bcc      = language.bcc
   subject  = language.subject
 
-#———————————————————————————————————————— missing fields
+#———————————————————————————————————————— send message
 
-  if addr == '' or body=='':
-    return HttpResponse('E0')
-
-#———————————————————————————————————————— validate email
-
-  whitelistmail = re.compile("[a-z,0-9,\.,_,-]+\@[a-z,0-9,\.,-]+\.[a-z]+")
-
-  if not whitelistmail.match(addr):
-    return HttpResponse('E1')
-
-#———————————————————————————————————————— validate name & body
-
-  blacklist = ".*[\\|\^|\$|\||\*|\+|\[|\{|<|>]+.*"
-
-  if re.match(blacklist, naim):
-    return HttpResponse('E2')
-
-  if re.match(blacklist, body):
-    return HttpResponse('E3')
-
-#———————————————————————————————————————— get name if not supplied
-
-  name = prettify(naim, addr)
-
-#———————————————————————————————————————— visible "from" address
-
-  frm = settings.url+ ' <' + settings.mail_id + '>'
-
-#———————————————————————————————————————— convert quote to harmless equivalent
-
-  body = re.sub('"', "''", body)
-  body = re.sub("'", "’" , body)
-  body = re.sub("`", "’" , body)
-
-#———————————————————————————————————————— from info in body
-
-# name is undefined in the following line
-
-  sender_info = language.mail_frm + ' ' + name + ' (' + addr + ')\n\n'
-  body = sender_info + body 
-
-#———————————————————————————————————————— send the message
-
-  response = send_mail.send(settings, subject, to, bcc, body)
-
-# if response == '': response = 'mail sent successfully'
-# response = '<html><body><pre>\n\n    ' + str(response)
-
+  message = stripQuotes(message)
+  response = send_mail.send(settings, subject, to, bcc, message)
   return HttpResponse(response)
 
 
 #———————————————————————————————————————— functions
 
-#———————————————————————————————————————— prettify(name, addr):
-
-def prettify(name, addr):
-
-  if name != '': return name
-
-  name, crap = addr.split('@')
-  name = re.sub("_", " ", name)
-  name = re.sub("\.", " ", name)
-  name = name.title() # http://www.codetable.net/Group/miscellaneous-symbols
-
-  return name
+def stripQuotes(str):
+  str = re.sub('"', "''", str)
+  str = re.sub("'", "’" , str)
+  str = re.sub("`", "’" , str)
+  return str
 
 
 #———————————————————————————————————————— fin
