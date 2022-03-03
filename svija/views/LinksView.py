9577da0b5cc1a6f1ac9ee3679e2c1ddd1b6205b0
[andrewcswift] <[andrew@svija.love]>
2022-03-03 10:11:37 +0100

    added red placeholder for missing images, changed 500 to 404 error for missing images

--- svija/views/LinksView.py
+++ svija/views/LinksView.py
@@ -5,7 +5,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-
+from os.path import exists
 from svija.models import Settings
 
 import os
@@ -14,20 +14,27 @@ sync_folder = os.path.abspath(os.path.dirname(__name__)) + '/sync/'
 
 def LinksView(request, request_prefix, placed_file):
 
-    # full path is in request.path for free
+    not_found = False
 
-    # home page image path ../../links/demo-only-big-29258056.jpg
-
-
-    source_dir = sync_folder + request_prefix + '/' + placed_file
+    img_path = sync_folder + request_prefix + '/' + placed_file
     bits = placed_file.split('.')
     type = bits[-1].lower()
     if type != 'png' and type != 'gif':
         type = 'jpeg'
 
+#———————————————————————————————————————— does file exist?
+
+    if not exists(img_path):
+      img_path = os.getcwd() + '/static/svija/images/ff0000.'+ type
+      not_Found = True
+
 #———————————————————————————————————————— return file contents
 
-    image_data = open(source_dir, "rb").read()
-    return HttpResponse(image_data, content_type='image/' + type)
+    image_data = open(img_path, "rb").read()
+    response = HttpResponse(image_data, content_type='image/' + type)
+    if not_found:
+      response.status_code = 404
+    return response
 
 #———————————————————————————————————————— fin
+
