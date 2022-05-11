e937664ad7b84f7acc7d017868ff16251bbcf80c
[andrewcswift] <[andrew@svija.love]>
2022-05-11 16:18:10 +0200

    always include scripts working, but modules broken

--- svija/views/modules/get_scripts.py
+++ svija/views/modules/get_scripts.py
@@ -59,23 +59,23 @@ def get_scripts(label, all_scripts):
       if this_script.active:
   
         if this_script.type == 'head JS':
-          hjc += get_script('js', this_script.name, this_script.content)
+          hjc = '\n\n//———————————————————————————————————————— ' + label + '\n\n'
           hj += get_script('js', this_script.name, this_script.content)
-   
+  
         if this_script.type == 'CSS':
-          hcc += get_script('css', this_script.name, this_script.content)
+          hcc = '\n\n/*———————————————————————————————————————— ' + label + ' */\n\n'
           hc += get_script('css', this_script.name, this_script.content)
-   
+  
         if this_script.type == 'body JS':
-          bjc += get_script('js', this_script.name, this_script.content)
+          bjc = '\n\n//———————————————————————————————————————— ' + label + '\n\n'
           bj += get_script('js', this_script.name, this_script.content)
-   
+  
         if this_script.type == 'HTML':
-          htc += get_script('html', this_script.name, this_script.content)
+          htc = '\n\n<!--—————————————————————————————————————— ' + label + ' -->\n\n'
           ht += get_script('html', this_script.name, this_script.content)
-   
+  
         if this_script.type == 'form':
-          fmc += get_script('html', this_script.name, this_script.content)
+          fmc = '\n\n<!--—————————————————————————————————————— ' + label + ' -->\n\n'
           fm += get_script('html', this_script.name, this_script.content)
   
 #———————————————————————————————————————— append iteration results
