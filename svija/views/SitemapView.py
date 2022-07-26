4b826baea7dd2ad7fabcb783bf602ea017db82e2
[andysvija] <[andy@svija.com]>
2022-07-26 15:53:19 +0200

    updated accessibility to remove section when it's the default

--- svija/views/SitemapView.py
+++ svija/views/SitemapView.py
@@ -1,47 +1,49 @@
 #———————————————————————————————————————— SitemapView.py
 #
-#        remove XML (1st two lines)
+#    see also modules/generate_accessibility.py
 #
-#        remove pixel dimensions if present
-#        equivalent of checking "responsive" in Illustrator
+#    remove XML (1st two lines)
 #
-#        add a unique ID based on filename if necessary
+#    remove pixel dimensions if present
+#    equivalent of checking "responsive" in Illustrator
 #
-#        change generic st classes to unique classes
-#        based on filename
+#    add a unique ID based on filename if necessary
+#
+#    change generic st classes to unique classes
+#    based on filename
 #
 #———————————————————————————————————————— import
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from svija.models import Page, Section, Settings
+from svija.models import Page, Settings
 
 def SitemapView(request):
-    settings = get_object_or_404(Settings,active=True)
-    domain = settings.url
+  settings = get_object_or_404(Settings,active=True)
+  domain = settings.url
 
-    default_code = settings.section.code
+  default_code = settings.section.code
 
-    response = sitemap(domain, default_code, Page.objects.all())
-    return HttpResponse(response, content_type='text/plain; charset=utf8')
+  response = sitemap(domain, default_code, Page.objects.all())
+  return HttpResponse(response, content_type='text/plain; charset=utf8')
 
 #———————————————————————————————————————— function
 
 def sitemap(domain, default_code, pages):
-    results = []
+  results = []
 
-    for page in pages:
-        if page.published == True:
-          page_code = page.section.code
-          if page_code == default_code:
-              addr = 'http://'+domain+'/'+page.url
-          else:
-              addr = 'http://'+domain+'/'+page_code+'/'+page.url
+  for page in pages:
+    if page.published == True:
+      page_code = page.section.code
+      if page_code == default_code:
+        addr = 'http://'+domain+'/'+page.url
+      else:
+        addr = 'http://'+domain+'/'+page_code+'/'+page.url
 
-          if page.url != 'missing':
-              results.append(addr)
+      if page.url != 'missing':
+        results.append(addr)
 
-    results = list(dict.fromkeys(results))
-    return '\n'.join(results)
+  results = list(dict.fromkeys(results))
+  return '\n'.join(results)
 
 #———————————————————————————————————————— fin
