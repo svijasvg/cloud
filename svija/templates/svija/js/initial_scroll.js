7792295cbd7d3baf15ac0a9c9fed11847aba508c
[andysvija] <[andy@svija.com]>
2022-11-02 16:05:52 +0100

    fixed horz scrollbar problem

--- svija/templates/svija/js/initial_scroll.js
+++ svija/templates/svija/js/initial_scroll.js
@@ -1,7 +1,6 @@
 //———————————————————————————————————————— template: initial_scroll.js
-
-// page_offsets set in admin
-// illustrator_pixel set in rem.js
+// makes centered over-width pages appear centered on load instead of centering late
+// page_offsets set in admin // illustrator_pixel set in rem.js
 
 //————— create full-width div
 
@@ -25,7 +24,8 @@ y = Math.round(top_margin_px);
 function testScroll(){ window.scrollTo(x, y); }
 setTimeout(testScroll, 1);
 
-//::::::::::::::::::::::::::::::::::::::::
+//:::::::::::::::::::::::::::::::::::::::: COPIED
+
 //———————————————————————————————————————— template: rem.js
 
 // visible_width is supplied by server
@@ -42,22 +42,23 @@ var illustrator_pixel = win_width / visible_width;
 //  in Safari, when someone pinches to zoom then reloads
 //  then reloads, this corrects the page size
 
-var firefox = navigator.userAgent.indexOf('Firefox');
-
-var pure = window.outerWidth/win_width*100;
-var current_zoom = Math.round(pure);
+var firefox = (navigator.userAgent.indexOf('Firefox') > 0);
+var zoom    = Math.round(window.outerWidth/win_width*100);
 
 // keyboard zoom levels, unlikely to be pinch levels. 300 is max pinch, so left out
 
-var zoom_levels = [50, 67, 75, 80, 85, 90, 100, 110, 125, 150, 170, 200, 240, 400, 500];
-var pinched = zoom_levels.indexOf(current_zoom) < 0 && firefox;
+var levels  = [50, 67, 75, 80, 85, 90, 100, 110, 125, 150, 170, 200, 240, 400, 500];
+var pinched = (firefox && typeof levels[zoom] != 'undefined');
 
-//if (pinched) illustrator_pixel = illustrator_pixel*current_zoom/100;
+if (pinched) illustrator_pixel = illustrator_pixel*zoom/100;
 
 //———————————————————————————————————————— set the rem unit
 
 document.documentElement.style.fontSize = illustrator_pixel + 'px';
 
-// makes centered over-width pages appear centered on load instead of centering late
+//:::::::::::::::::::::::::::::::::::::::: END COPY
+
+//———————————————————————————————————————— template: rem.js
+
 var pix_width = page_width * illustrator_pixel;
 initial_scroll_div.style.width = pix_width+'px';
--- svija/templates/svija/js/rem.js
+++ svija/templates/svija/js/rem.js
@@ -14,17 +14,15 @@ var illustrator_pixel = win_width / visible_width;
 //  in Safari, when someone pinches to zoom then reloads
 //  then reloads, this corrects the page size
 
-var firefox = navigator.userAgent.indexOf('Firefox');
-
-var pure = window.outerWidth/win_width*100;
-var current_zoom = Math.round(pure);
+var firefox = (navigator.userAgent.indexOf('Firefox') > 0);
+var zoom    = Math.round(window.outerWidth/win_width*100);
 
 // keyboard zoom levels, unlikely to be pinch levels. 300 is max pinch, so left out
 
-var zoom_levels = [50, 67, 75, 80, 85, 90, 100, 110, 125, 150, 170, 200, 240, 400, 500];
-var pinched = zoom_levels.indexOf(current_zoom) < 0 && firefox;
+var levels  = [50, 67, 75, 80, 85, 90, 100, 110, 125, 150, 170, 200, 240, 400, 500];
+var pinched = (firefox && typeof levels[zoom] != 'undefined');
 
-//if (pinched) illustrator_pixel = illustrator_pixel*current_zoom/100;
+if (pinched) illustrator_pixel = illustrator_pixel*zoom/100;
 
 //———————————————————————————————————————— set the rem unit
 
