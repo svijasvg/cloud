f284378bf5281ff9b234d4c3ebe70bad5730733e
[andysvija] <[andy@svija.com]>
2022-12-01 10:19:17 +0100

    added unsupported

--- svija/static/svija/js/unsupported.js
+++ svija/static/svija/js/unsupported.js
@@ -1,1 +1,14 @@
-alert('unsupported.js');
+<noscript>
+  <link rel="stylesheet" href="static/css/unsupported.css">
+  <p id="unsupported">
+
+  This website requires javascript. Please enable javascript and reload or visit with a different browser.
+
+  </p>
+</noscript>
+
+<script>
+
+/* test for no cookies or IE */
+
+</script>
