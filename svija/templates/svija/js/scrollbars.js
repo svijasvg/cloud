3d36349f941f6d033e297cc9a8ce0561f45d0119
[andysvija] <[andy@svija.com]>
2023-01-17 12:47:00 +0100

    separated out scrollbar compensation

---
+++ svija/templates/svija/js/scrollbars.js
@@ -0,0 +1,25 @@
+//———————————————————————————————————————— template: window_mgmt_body.js
+
+/*
+//———————————————————————————————————————— environmental variables
+
+var envPrevWidth = zoomedWidthBody();  // used in resize();
+var envPrevZoom  = zoom();             // used in resize();
+
+//———————————————————————————————————————— set the rem unit
+
+var aiPixel = zoomedWidthBody() / visible_width; // ⚠️  NEEDED IN OTHER SCRIPTS
+    aiPixel = aiPixel * zoom();
+
+document.documentElement.style.fontSize = aiPixel + 'px';
+
+//:::::::::::::::::::::::::::::::::::::::: methods
+
+//———————————————————————————————————————— zoomedWidthBody()
+
+function zoomedWidthBody(){
+  return document.documentElement.clientWidth;
+}
+
+//———————————————————————————————————————— fin
+*/
