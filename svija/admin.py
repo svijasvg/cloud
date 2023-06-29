a451a9fe4a858933acaa8523214862f12d9de3af
[andysvija] <[andy@svija.com]>
2023-06-29 13:39:12 +0200

    fixed checkbox spacing

--- svija/admin.py
+++ svija/admin.py
@@ -27,7 +27,7 @@ admin.site.register(Control, ControlAdmin)
 
 #———————————————————————————————————————— Redirect · no dependencies
 
-descRedirect = "Forward an old page to a new one, or to create shortcuts for frequently-visited pages · <a href=https://tech.svija.love/cloud/redirects target=_blank>documentation↑</a>"
+descRedirect = "Forward an old page to a new one, or to create shortcuts for frequently-visited pages · <a href=https://tech.svija.love/programs/cloud/redirects target=_blank>documentation↑</a>"
 
 from .models import Redirect
 class RedirectAdmin(admin.ModelAdmin):
@@ -45,7 +45,7 @@ admin.site.register(Redirect, RedirectAdmin)
 
 #———————————————————————————————————————— Font · no dependencies
 
-descFonts    = "Fonts are added here automatically the first time they are needed (page is reloaded) · <a href=https://tech.svija.love/cloud/fonts target=_blank>documentation↑</a>"
+descFonts    = "Fonts are added here automatically the first time they are needed (page is reloaded) · <a href=https://tech.svija.love/programs/cloud/fonts target=_blank>documentation↑</a>"
 
 from .models import Font
 class FontAdmin(admin.ModelAdmin):
@@ -65,7 +65,7 @@ admin.site.register(Font, FontAdmin)
 
 #———————————————————————————————————————— Section · no dependencies
 
-descSection  = "Website sections · see also <a href='/cloud/svija/screen/'>screen sizes</a> · <a href=https://tech.svija.love/cloud/sections target=_blank>documentation↑</a>"
+descSection  = "Website sections · see also <a href='/cloud/svija/screen/'>screen sizes</a> · <a href=https://tech.svija.love/programs/cloud/sections target=_blank>documentation↑</a>"
 
 from .models import Section
 class SectionAdmin(admin.ModelAdmin):
@@ -88,7 +88,7 @@ admin.site.register(Section, SectionAdmin)
 
 #———————————————————————————————————————— Screen · no dependencies
 
-descScreens    = "Supported screen sizes · for maximum pixel width, 0=unlimited · see also <a href='/cloud/svija/section/'>sections</a> · <a href=https://tech.svija.love/cloud/screens target=_blank>documentation↑</a>"
+descScreens    = "Supported screen sizes · for maximum pixel width, 0=unlimited · see also <a href='/cloud/svija/section/'>sections</a> · <a href=https://tech.svija.love/programs/cloud/screens target=_blank>documentation↑</a>"
 
 from .models import Screen
 class ScreenAdmin(admin.ModelAdmin):
@@ -108,7 +108,7 @@ admin.site.register(Screen, ScreenAdmin)
 
 #———————————————————————————————————————— Robots · no dependencies
 
-descRobots     = "Directives telling search engines whether or not to index your website · <a href='https://en.wikipedia.org/wiki/Robots_exclusion_standard'>wikipedia</a> · <a href=https://tech.svija.love/cloud/robots target=_blank>documentation↑</a>"
+descRobots     = "Directives telling search engines whether or not to index your website · <a href='https://en.wikipedia.org/wiki/Robots_exclusion_standard'>wikipedia</a> · <a href=https://tech.svija.love/programs/cloud/robots target=_blank>documentation↑</a>"
 
 from .models import Robots
 class RobotsAdmin(admin.ModelAdmin):
@@ -129,7 +129,7 @@ admin.site.register(Robots, RobotsAdmin)
 
 #———————————————————————————————————————— Script Set · no dependencies
 
-descScript0 = "Script Sets can also be included via <a href=\"/cloud/svija/page/\">page settings</a> · <a href=https://tech.svija.love/cloud/script-sets target=_blank>documentation↑</a>"
+descScript0 = "Script Sets can also be included via <a href=\"/cloud/svija/page/\">page settings</a> · <a href=https://tech.svija.love/programs/cloud/script-sets target=_blank>documentation↑</a>"
 descScript1 = "Link to instructions at <a href=\"https://tech.svija.love\">tech.svija.love</a> and usage notes"
 
 from .models import ScriptScripts
@@ -171,7 +171,7 @@ class ModuleScriptInline(admin.TabularInline):
   verbose_name_plural = "scripts"
   classes = ['collapse', 'ifempty',]
 
-descModules = "Reusable content that can be included here or via <b><a href='/cloud/svija/page/'>Page Settings</a></b> · <a href=https://tech.svija.love/cloud/modules target=_blank>documentation↑</a>"
+descModules = "Reusable content that can be included here or via <b><a href='/cloud/svija/page/'>Page Settings</a></b> · <a href=https://tech.svija.love/programs/cloud/modules target=_blank>documentation↑</a>"
 descDefaultY = "Link to instructions at <a href=\"https://tech.svija.love\">tech.svija.love</a> and usage notes"
 positdesc = 'Superimposed on the Illustrator page · negative = up ↖ left · positive = down ↘ right'
 
@@ -199,7 +199,7 @@ admin.site.register(Module, ModuleAdmin)
 
 #———————————————————————————————————————— Settings · depends on robots
 
-descSettings = "To request a different website address, please visit <a href='https://tech.svija.love/url' target='_blank'>tech.svija.love/url</a> · <a href=https://tech.svija.love/cloud/settings target=_blank>documentation↑</a>"
+descSettings = "To request a different website address, please visit <a href='https://tech.svija.love/url' target='_blank'>tech.svija.love/url</a> · <a href=https://tech.svija.love/programs/cloud/settings target=_blank>documentation↑</a>"
 
 from .models import Settings
 class SettingsAdmin(admin.ModelAdmin):
@@ -257,7 +257,7 @@ class AdditionalScriptInline(admin.TabularInline):
 #   classes = ['collapse']
 
 
-descPages  = "Settings that are specific to a single page · see also <a href='/cloud/svija/module/'>modules</a> · <a href=https://tech.svija.love/cloud/pages target=_blank>documentation↑</a>"
+descPages  = "Settings that are specific to a single page · see also <a href='/cloud/svija/module/'>modules</a> · <a href=https://tech.svija.love/programs/cloud/pages target=_blank>documentation↑</a>"
 descPixels = "Values are in pixels · Check \"Override default dimensions\" to activate"
 
 # https://stackoverflow.com/questions/16014719/adding-a-jquery-script-to-the-django-admin-interface
