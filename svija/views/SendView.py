307369be57d8b82d98a38c63f711881775fd039e
[andysvija] <[andy@svija.com]>
2025-09-08 16:39:11 +0200

    works w/o BCC

--- svija/views/SendView.py
+++ svija/views/SendView.py
@@ -18,34 +18,49 @@ from svija.models import Settings
 from modules import send_mail
 from django.http import QueryDict
 
+# from modules import send_mail       
+from django.core.mail import send_mail
+
 #———————————————————————————————————————— send mail
 
 def SendView(request):
 
   if not request.user.is_superuser:
-    response = HttpResponse(0)
+    response = HttpResponse("<h1>Error 404</h1>\nPage not found.")
     response.status_code = 404
     return response
 
   settings = get_object_or_404(Settings, enabled=True)
-
-  subject  = "⚠️ test email from " + settings.url
-  body   = 'If you have received this message, your email is working' 
-  frm    = ''
-  to     = request.GET.get('to', '')
-  cc     = ''
-  bcc    = request.GET.get('bcc', '')
+  to       = request.GET.get('to', '')
 
   if to == '':
     response =  "<pre>\n\n    Please include an email address:\n\n      " + settings.url + "/send?to=somebody@example.com&bcc=somebodyelse@website.com\n\n    bcc is optional"
     return HttpResponse(response)
 
-  response = send_mail.send(settings, subject, body, frm, [to], [cc], [bcc],)
+  subject  = "⚠️ test email from " + settings.url
+  body   = 'If you have received this message, your Svija email is working' 
+  frm    = ''
+  cc     = ''
+  bcc    = request.GET.get('bcc', '')
+  from_email     = "noreply@mail.svija.dev"
+  recipient_list = [request.GET.get('to', '')]
+  fail_silently  = False
 
-  if response == '': response = 'mail sent successfully'
-  response = '<html><body><pre>\n\n    ' + str(response)
+  response = send_mail(subject, body, from_email, recipient_list, fail_silently,)
 
-  return HttpResponse(response)
+  if (response == 1):
+    return HttpResponse("<pre>\n\n\n    Mail Sent.")
+  else:
+    return HttpResponse("Error: " + str(response))
 
+#———————————————————————————————————————— older content
+
+# response = send_mail.send(settings, subject, body, frm, [to], [cc], [bcc],)
+
+# if response == '': response = 'mail sent successfully'
+# response = '<html><body><pre>\n\n    ' + str(response)
+
+# return HttpResponse(response)
 
 #———————————————————————————————————————— fin
+
